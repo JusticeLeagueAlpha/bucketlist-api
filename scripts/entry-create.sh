fcd2755b42d01e75b35f95dc5269862f3f50d673
natGalynker <natalgal@gmail.com>
2016-09-02 18:44:42 -0400

    Test Show Action
    create show actions in routes
    create show curl request .sh

--- scripts/entry-create.sh
+++ scripts/entry-create.sh
@@ -7,6 +7,10 @@ curl --include --request POST http://localhost:3000/entries \
   --header "Authorization: Token token=uTJcKsk5Jtw6R7YACCE1DlijkxLI5CKNx2Y5RuhzZE4=--BOpsjHrYPu5puTUMzgVBxcKs+DRDf9kLU/7TmJNe0nM=" \
   --data '{
     "entry": {
-      "goal": "screw a kangaroo"
+      "goal": "screw a kangaroo",
+      "description": "Go overseas",
+      "finishBy": "2020-08-19",
+      "location": "Australia",
+      "url": "http://www.weekendnotes.com/spot-a-kangaroo-around-brisbane"
     }
   }'
