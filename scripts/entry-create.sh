850dc601b8cdb0f2c3a5f696ec02e0cd5b3b75ee
jordanallain <jallain@gmail.com>
2016-09-04 00:33:05 -0400

    Curl request for indexing user's entries
    Write curl request to test getting every entry the current
    user created.

--- scripts/entry-create.sh
+++ scripts/entry-create.sh
@@ -4,12 +4,12 @@
 
 curl --include --request POST http://localhost:3000/entries \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=BuBYtfA9Zbd8UpBkGO+rju1B7OMZSs9w2ZqVMcGNH54=--w8c3vwHNh6cbcz29dgcnNb29aIiLYIM/TSuaowGzwac=" \
+  --header "Authorization: Token token=naixWEVhxUWOppnZuD1vHIpnX+co6EW5GqaSt0abb88=--IUCt8/svJjYS+Oe048+tAXHV544tBmBvTUKxUWFhCY4=" \
   --data '{
     "entry": {
-      "goal": "catch a pikachu",
-      "description": "never let him out of his pokeball",
-      "finishBy": "2016-10-10",
-      "location": "Pallet Town"
+      "goal": "listen to Jordan chew",
+      "description": "listen to Jordan chew",
+      "finishBy": "ASAP",
+      "location": "General Assembly"
     }
   }'
