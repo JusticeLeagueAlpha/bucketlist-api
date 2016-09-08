08483c626c390b15af6977f3c18569feed9b91d7
jordanallain <jallain@gmail.com>
2016-09-07 21:13:13 -0400

    Alter URL of upload to file name
    Grab the name of the original file and assign it to the url of
    the upload so we can append it to the bucket url to display in
    handlebars.

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -11,7 +11,6 @@ const loader = require('lib/wiring/loader');
 
 const corsOptions = {
   origin: process.env.CLIENT_ORIGIN || 'https://justiceleaguealpha.github.io',
-    // origin: process.env.CLIENT_ORIGIN || 'http://localhost:8080',
 };
 
 const before = (app) => {
